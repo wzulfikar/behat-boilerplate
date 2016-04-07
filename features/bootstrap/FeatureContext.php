80e26125c5e6bc6d426b86b97d2ead6b95db96a6
wzulfikar <wzulfikar@ymail.com>
2016-04-07 13:39:02 +0800

    removed unused file require

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,34 +7,41 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
-// Require 3rd-party libraries here:
-require_once 'PHPUnitAssertFunctions.php';
+// third-party lib
+// require_once 'PHPUnit/Autoload.php';
+// require_once 'PHPUnit/Framework/Assert/Functions.php';
+
+// include helper for simple assert
+require_once 'helpers/assert.php';
+
+// include file which contains function we want to test
+// require_once 'src/filename.php';
 
 /**
- * Features context.
- */
+* Features context.
+*/
 class FeatureContext extends BehatContext
 {
-    /**
-     * Initializes context.
-     * Every scenario gets its own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
-    }
+  /**
+   * Initializes context.
+   * Every scenario gets its own context object.
+   *
+   * @param array $parameters context parameters (set them up through behat.yml)
+   */
+  public function __construct(array $parameters)
+  {
+    // Initialize your context here
+  }
+  
+//
+//  Place your definition and hook methods here:
+//
+//  /**
+//   * @Given /^I have done something with "([^"]*)"$/
+//   */
+//  public function iHaveDoneSomethingWith($argument)
+//  {
+//    doSomethingWith($argument);
+//  }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
 }
